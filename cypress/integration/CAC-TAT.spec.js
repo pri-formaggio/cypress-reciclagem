9140de83f502284a08a9c5dbe8ce22c2fab0e054
priscila.formaggio <priscila.formaggio@clicksign.com>
2022-08-28 16:50:48 -0300

    Create cypress project

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -176,7 +176,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     // Exercício 21: 
-    it.only('acessa a página da política de privacidade removendo o target e então clicanco no link', function(){
+    it('acessa a página da política de privacidade removendo o target e então clicanco no link', function(){
         cy.get('#privacy a').invoke('removeAttr', 'target').click()
         cy.contains('Talking About Testing')
     })
